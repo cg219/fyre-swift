2e9e6fcf0d50bdcfc3c1ea6bfaf35fa1fe028a4a
Clemente <dev@imkreative.com>
2020-07-07 20:44:14 -0400

    Initial Commit

---
+++ Fyre/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Fyre
+//
+//  Created by Clemente Gomez on 7/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fyre/FyreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FyreApp.swift
+//  Fyre
+//
+//  Created by Clemente Gomez on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct FyreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
